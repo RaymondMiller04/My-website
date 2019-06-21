e8af343987e5024ce34840fce617f0b95ad7486f
RaymondMiller04 <51975307+RaymondMiller04@users.noreply.github.com>
2019-06-21 14:15:42 -0700

    Update and rename javascript to javascript.js

--- javascript.js
+++ javascript.js
@@ -1,15 +1,13 @@
-//comments this is a whole function making stuff work
 function toggleProjects(){
   var projectList = document.getElementById("projectList");
   if(projectList.style.display == 'none'){
     projectList.style.display = 'block'
-    //The display property sets or returns the element's display type.
   }
   else{
     projectList.style.display = 'none'
   }
 }
-//
+
 function toggleTech(){
   var techList = document.getElementById("techList");
   if(techList.style.display == 'none'){
